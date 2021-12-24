5b7c21a8a51ea8b9671ec8e607e34d4d4f881976
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-24 20:33:23 +0300

    Scripted auto-commit on change (2021-12-24 20:33:23) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/HttpServer.java
+++ src/main/java/ru/bmstu/ru/HttpServer.java
@@ -3,6 +3,7 @@ package ru.bmstu.ru;
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
@@ -23,13 +24,18 @@ public class HttpServer {
     public static final String LOCALHOST = "localhost";
     public static final String COUNT = "count";
     public static final String DEFAULT_COUNT = "1";
-    ActorSystem actorSystem = ActorSystem.create("routes");
+    public static final String STORAGE = "storage";
+
+    private ActorSystem actorSystem;
+    private ActorRef storage;
 
     public HttpServer() {
-
+        actorSystem = ActorSystem.create("routes");
+        storage = actorSystem.actorOf(Props.create(StorageActor.class), STORAGE);
     }
 
     public void run() throws IOException {
+
         final Http http = Http.get(actorSystem);
         final ActorMaterializer actorMaterializer = ActorMaterializer.create(actorSystem);
         AsyncHttpClient client = asyncHttpClient();
