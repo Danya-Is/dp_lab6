eebf38e25242470bc8fe0bc1735cc6076e948024
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-24 21:53:31 +0300

    Scripted auto-commit on change (2021-12-24 21:53:31) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/NodeHandler.java
+++ src/main/java/ru/bmstu/ru/NodeHandler.java
@@ -1,2 +1,16 @@
-package ru.bmstu.ru;public class NodeHandler {
+package ru.bmstu.ru;
+
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
+
+public class NodeHandler {
+    private ZooKeeper zoo;
+    private ActorRef storage;
+    private String path;
+
+    public NodeHandler(ZooKeeper zoo, ActorRef storage, String path) {
+        this.path = path;
+        this.zoo = zoo;
+        this.storage = storage;
+    }
 }
