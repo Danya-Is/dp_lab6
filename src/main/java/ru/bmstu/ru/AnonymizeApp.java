3212320c11ea6a5b179ef140e4e5abfd66d24a08
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-24 19:59:13 +0300

    Scripted auto-commit on change (2021-12-24 19:59:13) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/AnonymizeApp.java
+++ src/main/java/ru/bmstu/ru/AnonymizeApp.java
@@ -3,7 +3,8 @@ package ru.bmstu.ru;
 public class AnonymizeApp {
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.out.println("Usage: <>");
+            System.out.println("Usage: <Host> <Port>");
+            System.exit(1);
         }
     }
 }
