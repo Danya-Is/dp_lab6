3b4a242bb3513a181c22eb3fd11c5bfcbdcc3b3f
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-24 20:05:07 +0300

    Scripted auto-commit on change (2021-12-24 20:05:07) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/AnonymizeApp.java
+++ src/main/java/ru/bmstu/ru/AnonymizeApp.java
@@ -1,12 +1,15 @@
 package ru.bmstu.ru;
 
+import java.io.IOException;
+
 public class AnonymizeApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             System.out.println("Usage: <Host> <Port>");
             System.exit(1);
         }
-    }
 
-    HttpServer server = new HttpServer();
+        HttpServer server = new HttpServer();
+        server.run();
+    }
 }
