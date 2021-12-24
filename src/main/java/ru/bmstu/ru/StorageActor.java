b424fc27cefb4706f3923625fc1dc5e88abbca54
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-24 22:10:43 +0300

    Scripted auto-commit on change (2021-12-24 22:10:43) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/StorageActor.java
+++ src/main/java/ru/bmstu/ru/StorageActor.java
@@ -1,23 +1,25 @@
 package ru.bmstu.ru;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import ru.bmstu.ru.Messages.GetRandomServerMessage;
 import ru.bmstu.ru.Messages.RandomServerMessage;
+import ru.bmstu.ru.Messages.ServersListMessage;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class StorageActor extends AbstractActor {
 
     private ArrayList<String> storage = new ArrayList<>();
+    private Random random = new Random();
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(GetRandomServerMessage.class, )
+                .match(GetRandomServerMessage.class, msg ->
+                        sender().tell(new RandomServerMessage(storage.get(random.nextInt(storage.size()))), ActorRef.noSender()))
+                .match(ServersListMessage.class, msg -> storage.addAll(msg.getServers()))
                 .build();
-    }
-
-    private void sendRandomServerMessage() {
-        sender().tell(new RandomServerMessage());
     }
 }
