abfa7589bf3897870b1bf8d2549bd74cc0951143
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-24 19:49:19 +0300

    Scripted auto-commit on change (2021-12-24 19:49:19) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/HttpServer.java
+++ src/main/java/ru/bmstu/ru/HttpServer.java
@@ -1,4 +1,12 @@
 package ru.bmstu.ru;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+
 public class HttpServer {
+    ActorSystem actorSystem = ActorSystem.create("routes");
+    final Http http = Http.get(actorSystem);
+    final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
+    
 }
