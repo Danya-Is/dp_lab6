39dd586d1db24f1b1b7892f8443509a9663158f1
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-24 21:36:49 +0300

    Scripted auto-commit on change (2021-12-24 21:36:49) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/StorageActor.java
+++ src/main/java/ru/bmstu/ru/StorageActor.java
@@ -1,11 +1,22 @@
 package ru.bmstu.ru;
 
 import akka.actor.AbstractActor;
+import ru.bmstu.ru.Messages.GetRandomServerMessage;
+
+import java.util.ArrayList;
 
 public class StorageActor extends AbstractActor {
 
+    private ArrayList<String> storage = new ArrayList<>();
+
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match(GetRandomServerMessage.class, )
+                .build();
+    }
+
+    private void sendRandomServerMessage() {
+        sender().tell();
     }
 }
