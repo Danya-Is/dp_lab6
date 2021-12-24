45b58a8683f51d4f4184d0d60865e3aa3f61da88
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-24 20:29:51 +0300

    Scripted auto-commit on change (2021-12-24 20:29:51) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/HttpServer.java
+++ src/main/java/ru/bmstu/ru/HttpServer.java
@@ -14,6 +14,7 @@ import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
+import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class HttpServer {
     private static final int PORT = 8888;
