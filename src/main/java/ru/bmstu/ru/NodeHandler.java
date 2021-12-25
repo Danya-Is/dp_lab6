e08768ffe4d04c4d84a3515a233a1161db027640
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-25 09:25:56 +0300

    Scripted auto-commit on change (2021-12-25 09:25:55) by gitwatch.sh

--- src/main/java/ru/bmstu/ru/NodeHandler.java
+++ src/main/java/ru/bmstu/ru/NodeHandler.java
@@ -5,6 +5,7 @@ import org.apache.zookeeper.*;
 import ru.bmstu.ru.Messages.ServersListMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class NodeHandler implements Watcher{
@@ -35,17 +36,11 @@ public class NodeHandler implements Watcher{
 
     private void watchChildren(WatchedEvent event) {
         try{
-            storage.tell(new ServersListMessage(zooKeeper.getChildren(path, this::watchChildren).stream()
-                    .map(subPath -> createPath(path, subPath))
-                    .collect(Collectors.toList())), ActorRef.noSender());
+            storage.tell(new ServersListMessage(new ArrayList<>(zooKeeper.getChildren(path, this::watchChildren))), ActorRef.noSender());
         } catch (InterruptedException| KeeperException e) {
             throw new RuntimeException(e);
         }
 
-    }
-
-    private String createPath(String path, String subPath) {
-        return path + "/" + subPath;
     }
 
     @Override
